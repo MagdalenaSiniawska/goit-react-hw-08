ef7438d54be2e1a534b4649dd6bde60969e657fd
MagdalenaSiniawska <m.paszke94@gmail.com>
2024-12-17 01:03:59 +0100

    homework hw8

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import { Suspense } from 'react';
+import styles from './Layout.module.css';
+import AppBar from '../AppBar/AppBar';
+
+const Layout = ({ children }) => {
+	return (
+		<div>
+
+			<AppBar className={styles.barContainer} />
+			<main className={styles.pageContainer}>
+				<Suspense fallback={null}>{children}</Suspense>
+			</main>
+		</div>
+	);
+};
+
+export default Layout;
